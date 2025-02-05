00008d7ed6e2a020cd3828a2df5842ab75a189a3
Shraey Chikker <shraeychikker@Apollo-Shraey-Chikkers-MacBook-Pro.local>
2025-02-05 19:29:11 +0530

    config change

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: "export",
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },
 };
 
 module.exports = nextConfig;
